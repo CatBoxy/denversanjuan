aa1102271a04ab6aa3e4b663e37609c424eccb46
CatBoxy <thecatbox@protonmail.com>
2022-04-21 10:21:51 -0300

    Home styles + button

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import useCurrency from '../hooks/useCurrency';
 import PriceBox from '../components/PriceBox';
+import { BsWhatsapp } from "react-icons/bs";
+
 
 
 function Home() {
   const { currency, isLoading} = useCurrency();
   return (
+    <>
     <div className='home'>
       {isLoading ? <span>Loading...</span> : <Display currency={currency} />}
     </div>
+    <section className='buttonSection'>
+      <a className='wspText'>
+        <button className='wspButton'>
+          <BsWhatsapp className='wspIcon'/>
+          Compartir 
+        </button>
+      </a>
+    </section>
+    </>
   );
 }
 
@@ -17,10 +29,10 @@ export default Home;
 function Display({ currency }) {
   return (
     <>
-      <div>
-        <PriceBox name={currency[0].name} compra={currency[0].compra} venta={currency[0].venta} />
-        <PriceBox name={currency[1].name} compra={currency[1].compra} venta={currency[1].venta}/>
-        <PriceBox name={currency[2].name} compra={currency[2].compra} venta={currency[2].venta}/>
+      <div className='bodyDisplay'>
+        <PriceBox id="usd" name={currency[0].name} compra={currency[0].compra} venta={currency[0].venta} />
+        <PriceBox id="eur" name={currency[1].name} compra={currency[1].compra} venta={currency[1].venta}/>
+        <PriceBox id="usdt" name={currency[2].name} compra={currency[2].compra} venta={currency[2].venta}/>
       </div>
     </>
   )
