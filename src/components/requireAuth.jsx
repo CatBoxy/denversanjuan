8bb26073d3b52aa2473de18338df97c500f1d82c
CatBoxy <thecatbox@protonmail.com>
2022-04-22 11:06:10 -0300

    typo

--- src/components/requireAuth.jsx
+++ src/components/requireAuth.jsx
@@ -4,6 +4,7 @@ import { Navigate, useLocation } from 'react-router-dom';
 
 export default function RequireAuth({ children }) {
   const { isLoggedin } = useAuthState();
+  
   const location = useLocation();
 
   // Redirect to /login page, but save the current location they were trying to go to when they where redirected
