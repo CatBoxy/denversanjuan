0bd547367ca5c4832cc7abbd0526139022f41698
CatBoxy <thecatbox@protonmail.com>
2022-04-22 20:02:14 -0300

    dates fix + styles

--- src/hooks/useDate.js
+++ src/hooks/useDate.js
@@ -6,14 +6,26 @@ export default function useDate({ createdAt }) {
   useEffect(() => { // formats firebase date to js date format
     const formatDate = () => {
       const date = createdAt.toDate();
-      const year = new Date(date).getFullYear()
-      const month = new Date(date).getMonth() + 1
-      const day = new Date(date).getDate()
-      const hours = new Date(date).getHours()
-      const minutes = new Date(date).getMinutes()
+      const year = new Date(date).getFullYear();
+      const month = new Date(date).getMonth() + 1;
+      const day = new Date(date).getDate();
+      const hours = new Date(date).getHours();
+      const minutes = new Date(date).getMinutes();
 
-      const newDate = day + '/' + month + '/' + year + ' - ' + hours + ':' + minutes + ' hs';
-      setFormattedDate(newDate)
+      let newHours = hours;
+      let newMinutes = minutes;
+
+      if (hours < 10) {
+        newHours = '0' + hours;
+      };
+
+      if (minutes < 10) {
+        newMinutes = '0' + minutes;
+      };
+
+      const newDate = day + '/' + month + '/' + year + ' - ' + newHours + ':' + newMinutes + ' hs';
+      console.log(newDate)
+      setFormattedDate(newDate);
 
     };
     formatDate();
